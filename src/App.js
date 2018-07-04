0c6f9b702b37280da9b389cdad6d0b7a4e76bb5f
Sigit Kuncoro <sigitcountzoro@gmail.com>
2018-07-04 23:33:36 +0700

    Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from "react-redux";
 import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
@@ -10,9 +11,11 @@ import ResetPasswordPage from './components/pages/ResetPasswordPage';
 import DashboardPage from './components/pages/DashboardPage';
 import UserRoute from './components/routes/UserRoute';
 import GuestRoute from './components/routes/GuestRoute';
+import TopNavigation from './components/navigation/TopNavigation';
 
-const App = ({ location }) => (
+const App = ({ location, isAuthenticated }) => (
 <div className='ui container'>
+  {isAuthenticated && <TopNavigation/>}
   <Route location={location} path='/' exact component={HomePage}/>
   <Route location={location} path='/confirmation/:token' exact component={ConfirmationPage}/>
   <GuestRoute location={location} path='/login' exact component={LoginPage}/>
@@ -27,6 +30,13 @@ App.protoType = {
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired,
   }).isRequired,
+  isAuthenticated: PropTypes.bool.isRequired,
 }
 
-export default App;
+function mapStateToProps(state){
+  return {
+    isAuthenticated: !!state.user.email
+  }
+}
+
+export default connect(mapStateToProps)(App);
